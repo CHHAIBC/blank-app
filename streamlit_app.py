79abb7639cc30d7004a89747be320b76aee8edbd
CHHAIBC <chh.aibootcamp@gmail.com>
2024-10-22 11:30:22 +0000

    Added Secret

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 My new Streamlit app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+# Everything is accessible via the st.secrets dict:
+st.write("OPENAPI_API_KEY:", st.secrets["OPENAPI_API_KEY"]) 
+
+# Store the value in "Environment Variable"
+# This is required by some packages like LangChain
+os.environ['OPENAPI_API_KEY'] = st.secrets['OPENAPI_API_KEY']
